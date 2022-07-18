a38087df5a53e9b077db524f06a95d6158c9e970
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-07-18 18:16:18 +1200

    lab 2 in working

---
+++ labs/lab 1/Q2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q2 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter principle:");
+    double p = input.nextDouble();
+
+    System.out.println("Enter time (number of years):");
+    double t = input.nextDouble();
+
+    System.out.println("Enter rate:");
+    double r = input.nextDouble();
+
+    double answer = (p*t*r)/100;
+
+    System.out.printf("Simple Interest = $%.2f",answer);
+    
+
+
+  }
+}
