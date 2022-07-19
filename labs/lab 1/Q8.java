4399abf68f74d401452326375263cf3216ecf828
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-07-19 14:57:23 +1200

    update

---
+++ labs/lab 1/Q8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Q8 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first = input.nextLine();
+
+        System.out.println("Enter your surname:");
+        String last = input.nextLine();
+
+        System.out.println("Enter a number between 1 and 999 (both inclusive):");
+        int num = input.nextInt();
+        
+
+
+        // String first = "Damir";         
+        // String last = "Azhar";
+        // int num = 1;
+
+        first = first.toLowerCase();
+        String listFirst[] = first.split("");
+        last = last.toLowerCase();
+        String listLast[] = last.split("");
+        
+        
+        System.out.printf("Your username is: %s%s%s%s%03d\n",listFirst[0],listLast[0],listLast[1],listLast[2],num);
+        
+        
+        
+
+
+ } 
+} 
--- labs/lab 1/README.md
+++ labs/lab 1/README.md
@@ -1,3 +1,16 @@
 ### FIRST LAB
 
 basic java stuff, some things were tricky to figure out such as java.time.LocalDate (Q6) but thanks to a bit of googling and helpfull codetips in vs we struggled through it! 
+
+## things to rem 
+
+<pre>
+import java.util.Scanner;
+public class Q8 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first = input.nextLine();
+
+</pre>
