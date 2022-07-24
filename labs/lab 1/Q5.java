4f4c06021dbd0b6a5bbc84f718787aa75836e54b
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-07-25 11:06:50 +1200

    5 done

--- labs/lab 1/Q5.java
+++ labs/lab 1/Q5.java
@@ -2,16 +2,46 @@ import java.util.Scanner;
 
 public class Q5 {
 	public static void main(String[] args) {
-		// Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
         
-		double lon1 = -36.8404;
-		double lat1 = -174.7399;
+		System.out.println("Enter the latitude of city1:");
+		double lat1 = input.nextDouble();
 		
-		double lon2 = -33.8600;
-		double lat2 = -151.2094;
+
+		System.out.println("Enter the longitude of city1:");
+		double lon1 = input.nextDouble();
+
+		System.out.println("Enter the latitude of city2:");
+		double lat2 = input.nextDouble();
+
+		System.out.println("Enter the longitude of city2:");
+		double lon2 = input.nextDouble();
 		
+		lat1 = Math.toRadians(lat1);
+		lon1 = Math.toRadians(lon1);
+
+		lat2 = Math.toRadians(lat2);
+		lon2 = Math.toRadians(lon2);
+
+
+		// double lon1 = Math.toRadians(-174.7399);
+		// double lat1 = Math.toRadians(-36.8404);
+		
+		// double lon2 = Math.toRadians(-151.2094);
+		// double lat2 = Math.toRadians(-33.8600);
 		
 
 
+		double sinLat = Math.sin((lat2-lat1)/2);
+		sinLat = Math.pow(sinLat, 2.0);
+		
+		double sinLon = Math.sin((lon2-lon1)/2);
+		sinLon = Math.pow(sinLon, 2.0);
+
+		double asin = Math.asin(Math.sqrt((sinLat +( Math.cos(lat1) * Math.cos(lat2) * sinLon))));
+
+		double result = (2*6371)*asin;
+
+		System.out.printf("The distance between the two cities is %.02fkilometres.\n",result);
  } 
 } 
