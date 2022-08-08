e57365bef649596243012560375aa9027aa64750
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-08-09 11:04:19 +1200

    Start assingment

---
+++ assingments/A1code/A1.java
@@ -0,0 +1,25 @@
+/*
+ *  =====================================================================================================
+ *  A1.java : Extends JFrame and contains a panel where shapes move around on the screen (entire project)
+ *  YOUR UPI: ANSWER
+ *  =====================================================================================================
+ */
+
+import java.util.ArrayList;
+import java.awt.*;
+public class A1 {
+	private AnimationViewer panel;  // panel for bouncing area
+	public static void main(String[] args) {
+		A1 program = new A1();
+	}
+	public A1() {
+		panel = new AnimationViewer();
+		Graphics g = null;
+		for (int i=0; i<10; i++) {
+			panel.createNewShape(i, i+5);
+			panel.paintComponent(g);
+		}
+		System.out.println("DONE");
+	}
+}
+
