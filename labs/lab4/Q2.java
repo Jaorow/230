cf170cb8a2adc3e39a2cb62689c0710e456b46a7
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-08-12 17:16:03 +1200

    fix 2

--- labs/lab4/Q2.java
+++ labs/lab4/Q2.java
@@ -1,59 +1,42 @@
 class Sentence{
-    private String text = "ab ";
-    private String[] vow = {"a","A","e","E","i","I","o","O","u","U"};
-    private int numSpaces = NumSpaces(),numVowels = NumVowels(),numConsonants = NumConsonants();
+    private String text = "";
+    private String con = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtWwVvXxYyZz";
+    private String vow = "aAeEiIoOuU";
+    private int numSpaces,numVowels,numConsonants;
     
     public Sentence(){
         text = "";
     }
     public Sentence(String sent){
         text = sent;
-    }
+        String[] split_string = sent.split("");
+        
+        for (String letter : split_string) {
 
-    private int NumSpaces(){
-        String[] arr = text.split("");
+            
 
-        for (String letter : arr) {
-            if(letter == " "){
-            numSpaces ++;
+            if(con.contains(letter)){
+                numConsonants++;
             }
+
+            else if(vow.contains(letter)){
+                numVowels++;
+            }else{numSpaces++;}
+   
         }
-        return numSpaces;
     }
-
-
-    private int NumVowels(){
-        String[] arr = text.split("");
-
-        for (String i : arr) {
-            for (String v : vow) {
-                if (i == v) {
-                    numVowels ++;
-                }
-            }
-        }
-        return numVowels;
-    }    
-    private int NumConsonants(){
-        // int Vowels = getNumVowels();
-        // int spaces = getNumSpaces();
-        int length = text.length();
-        numConsonants = length - numSpaces - numVowels;
-        return numConsonants;
-    }
+    
     public int getNumVowels(){return numVowels;}
     public int getNumConsonants(){return numConsonants;}
     public int getNumSpaces(){return numSpaces;}
 
     public double getVowelsToConsonantsRatio(){
-        int Vowels = numVowels;
-        int cons = numConsonants;
-        return Vowels/cons;
+        double i = numVowels;
+        double j =numConsonants;
+        return i/j;
     }
+    
     public String toString(){
-        // int sVowels = getNumVowels();
-        // int sspaces = getNumSpaces();
-        // int scons = getNumConsonants();
         String ret = String.format("%s(spaces=%d,vowels=%d,consonants=%d)",text,numSpaces,numVowels,numConsonants);
         return ret;
     }
