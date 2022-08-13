20a5b3b90282ee7fc33fbc1e3d13e7d2cc352778
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-08-13 22:03:50 +1200

    update defults

--- assingments/A1code/AnimationViewer.java
+++ assingments/A1code/AnimationViewer.java
@@ -2,7 +2,7 @@
  * ==========================================================================================
  *  AnimationPanel.java : Moves shapes around on the screen according to different paths.
  *  It is the main drawing area where shapes are added and manipulated.
- *  YOUR UPI: ANSWER
+ *  YOUR UPI: Jdun349
  *  =========================================================================================
  */
 
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 
 class AnimationViewer  {
     private ArrayList<Shape> shapes = new ArrayList<Shape>(); //create the ArrayList to store shapes
-    private int currentShapeType=Shape.DEFAULT_SHAPETYPE; // the current shape type,
-    private int currentPathType=Shape.DEFAULT_PATHTYPE;  // the current path type
+    private ShapeType currentShapeType=Shape.DEFAULT_SHAPETYPE; // the current shape type,
+    private PathType currentPathType=Shape.DEFAULT_PATHTYPE;  // the current path type
     private Color currentColor=Shape.DEFAULT_COLOR;  // the current fill colour of a shape
     private int currentPanelWidth=Shape.DEFAULT_PANEL_WIDTH, currentPanelHeight = Shape.DEFAULT_PANEL_HEIGHT, currentWidth=Shape.DEFAULT_WIDTH, currentHeight=Shape.DEFAULT_HEIGHT;
 
