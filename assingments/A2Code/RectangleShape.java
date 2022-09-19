4a5075945ced39f92df88def436deff4108f1380
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-09-20 10:15:08 +1200

    setup shape

--- assingments/A2Code/RectangleShape.java
+++ assingments/A2Code/RectangleShape.java
@@ -6,8 +6,8 @@
 import java.awt.*;
 class RectangleShape extends Shape {
     public RectangleShape() {}
-	public RectangleShape(int x, int y, int w, int h, int mw, int mh, Color c, PathType pt) {
-		super(x ,y ,w, h ,mw ,mh, c, pt);
+	public RectangleShape(int x, int y, int w, int h, int mw, int mh, Color c, PathType pt,String t) {
+		super(x ,y ,w, h ,mw ,mh, c, pt,t);
 	}
 	@Override
 	public void draw(Graphics g) {
