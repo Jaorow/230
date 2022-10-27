9e44d6a55e4c6ec3c2e44d5ad51382fe2ac68f08
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-10-27 19:50:02 +1300

    banner

---
+++ exams/revision/Banner.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Banner {
+    private String word;
+    private int width;
+    public Banner(){
+        this.word = "";
+        this.width = 4;
+    }
+    public Banner(String word){
+        this.word = word;
+        this.width = this.word.length()+4;
+    }
+    public Banner(String word, int width){
+        this.word = word;
+        if (width >= this.word.length()+4) {
+            this.width = width;
+        } else { 
+            this.width = this.word.length()+4;
+        }
+    }
+
+    public String toString(){
+        String firstAndLast = "";
+        for (int i = 0; i < width; i++) { firstAndLast += "*"; }
+
+        String middle = "*";
+        String spaces = "";
+        for (int i = 0; i < ((width-word.length())/2)-1; i++) {
+            spaces += " ";
+        }
+        middle += spaces; middle += word; middle += spaces;
+        if (middle.length() != firstAndLast.length()-1){
+            middle += " *";
+        } else { middle += "*"; }
+        return String.format("%s\n%s\n%s",firstAndLast,middle,firstAndLast);
+    }
+
+    public int getWidth(){ return this.width; }
+    public void setWidth(int width){         
+        if (width >= this.word.length()+4) {
+            this.width = width;
+        } else { 
+            this.width = this.word.length()+4;
+        } 
+    }
+}
+
+class Container{
+    private ArrayList<Banner> words = new ArrayList<Banner>();
+    private int width;
+    public Container(){};
+    public void add(Banner word){
+        if(word.getWidth() <= width){
+            word.setWidth(width);
+            words.add(word);
+        } else {
+            for (Banner banner : words) {
+                banner.setWidth(width);
+            }
+        }
+    }
+}
